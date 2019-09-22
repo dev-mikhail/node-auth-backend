0e4b749827536c603cb5dd9e8665266fb57833fd
Mikhail <mikhail97ru@rambler.ru>
2019-09-23 03:28:16 +0800

    completed the backend with Node.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./db');
 
+const users = require('./routes/user');
+
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database' + err) }
@@ -12,8 +14,13 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 
 const app = express();
 
+app.use(passport.initialize());
+require('./passport')(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api/users', users);
 
 app.get('/', function (req, res) {
   res.send('hello');
--- routes/user.js
+++ routes/user.js
@@ -11,10 +11,10 @@ const validateLoginInput = require('../validation/login');
 const User = require('../models/User');
 
 router.post('/register', function (req, res) {
-  const { error, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) {
-    retrun res.status(400).json(errors);
+    return res.status(400).json(errors);
   }
   User.findOne({
     email: req.body.email
